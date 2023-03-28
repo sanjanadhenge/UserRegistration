f3df061677ebc0b4e7610e04fb9837520044ddec
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-28 18:45:26 +0530

    [sanjana] Add : First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            User user = new User();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("Select option \n 1. First Name \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name");
+                        string first = Console.ReadLine();
+                        user.ValidateFirstName(first);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
--- UserRegistration/User.cs
+++ UserRegistration/User.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
 {
     internal class User
     {
+        public static string NAME_REGX = "^[A-Z]{1}[a-z]{2,}$";
+        public void ValidateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME_REGX))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+        }
     }
 }
