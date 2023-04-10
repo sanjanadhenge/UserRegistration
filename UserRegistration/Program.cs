e45fe12678a0f853e521f26e5b58e86735b8da57
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-10 14:23:55 +0530

    [sanjana] Add : Using Lamda Functions

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -12,6 +12,7 @@ namespace UserRegistration
         public static void Main(string[] args)
         {
             User user = new User();
+            UsingLambdaFunction usingLambdaFunction = new UsingLambdaFunction();
             bool flag = true;
             while(flag)
             {
@@ -22,25 +23,36 @@ namespace UserRegistration
                     case 1:
                         Console.WriteLine("Enter First Name");
                         string first = Console.ReadLine();
-                        user.ValidateFirstName(first);
+                        //user.ValidateFirstName(first);
+                      bool flag2=  usingLambdaFunction.ValidFirstNameLamba(first);
+                        Console.WriteLine(flag2);
                         break;
                     case 2:
                         Console.WriteLine("Enter Last Name");
                         string last = Console.ReadLine();
-                        user.ValidateLastName(last);
+                        // user.ValidateLastName(last);
+                       bool flag3= usingLambdaFunction.ValidLastNameLamba(last);
+                       Console.WriteLine(flag3);
                         break;
                     case 3:
                         Console.WriteLine("Enter Mobile Number");
                         string Number = Console.ReadLine();
-                        user.ValidateMobile(Number);
+                        bool flag4= usingLambdaFunction.ValidMobileLamba(Number);
+                        // user.ValidateMobile(Number);
+                        Console.WriteLine(flag4);
                         break;
                     case 4:
-                        user.ValidateMailID();
+                        Console.WriteLine("Enter Mail ID");
+                        string Mail = Console.ReadLine();
+                        bool flag5 = usingLambdaFunction.ValidMailLamba(Mail);
+                        Console.WriteLine(flag5);
                         break;
                     case 5:
                         Console.WriteLine("Enter Password");
                         string pass = Console.ReadLine();
-                        user.ValidatePass(pass);
+                        //user.ValidatePass(pass);
+                        bool flag6 = usingLambdaFunction.ValidPasswordLamba(pass);
+                        Console.WriteLine(flag6);
                         break;
                     case 6:
                         flag = false;
---
+++ UserRegistration/UsingLambdaFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class UsingLambdaFunction
+    {
+        public static string NAME_REGX = "^[A-Z]{1}[a-z]{2,}$";
+        public static string MOBILENUMBER = "^[6-9]{1}[0-9]{9}$";
+        public static string Mail_ID = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
+        public static string Password = "^[A-Z]{1,}[a-z]{5,}[!@#%&*~$]{1}[0-9]{1,}$";
+        public bool ValidFirstNameLamba(string firstName)=>Regex.IsMatch(firstName, NAME_REGX) ? true: false;
+        public bool ValidLastNameLamba(string lastName) => Regex.IsMatch(lastName, NAME_REGX) ? true : false;
+        public bool ValidMobileLamba(string Mobile) => Regex.IsMatch(Mobile, NAME_REGX) ? true : false;
+        public bool ValidMailLamba(string Mail) => Regex.IsMatch(Mail, NAME_REGX) ? true : false;
+        public bool ValidPasswordLamba(string Pass) => Regex.IsMatch(Pass, NAME_REGX) ? true : false;
+
+    }
+}
